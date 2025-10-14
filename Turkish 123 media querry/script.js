d1d02ef0ada7e0522740487d8eaed6530c026822
Shahzaib <shazuthebigbrother@gmail.com>
2025-10-14 02:15:40 -0700

    Add files via upload

---
+++ Turkish 123 media querry/script.js
@@ -0,0 +1,36 @@
+const hbtn=document.querySelector('#h1')
+const heabtn=document.querySelector('.hea2')
+
+document.addEventListener('DOMContentLoaded', function() {
+
+hbtn.addEventListener('click',()=>{
+heabtn.scrollIntoView({behavior:'smooth'})
+
+})
+})
+
+
+
+const cbtnbtn=document.querySelector('#cb')
+const h1btn=document.querySelector('.hea1')
+
+document.addEventListener('DOMContentLoaded', function() {
+
+cbtnbtn.addEventListener('click',()=>{
+h1btn.scrollIntoView({behavior:'smooth'})
+
+})
+})
+
+
+
+const cb2nbtn=document.querySelector('#t123')
+const h2btn=document.querySelector('.main')
+
+document.addEventListener('DOMContentLoaded', function() {
+
+cb2nbtn.addEventListener('click',()=>{
+h2btn.scrollIntoView({behavior:'smooth'})
+
+})
+})
